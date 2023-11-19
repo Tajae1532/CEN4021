b25566481a650a0c38a8c6de490007d207573701
cair0c <cairocraw@gmail.com>
2023-11-19 03:11:17 -0500

    removing my api key LOL. have to figure something out later

--- ScheduleRequest.py
+++ ScheduleRequest.py
@@ -8,7 +8,7 @@ def getSchedule(week, sType, sYear):
     querystring = {"week": week, "seasonType": sType, "season": sYear}
 
     headers = {
-        "X-RapidAPI-Key": "31a0367006msh826038b6be6e9c0p10628fjsna8d24c75286b",
+        "X-RapidAPI-Key": "PUT SOMETHING HERE LATER DONT FORGET",
         "X-RapidAPI-Host": "tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com"
     }
 
