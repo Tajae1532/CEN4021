14dffb571e2b5604a862698fcf91992c53a49faa
cair0c <cairocraw@gmail.com>
2023-11-26 21:41:10 -0500

    Deleted convertGameDate.py and added functionality to ExtractInfoFromResponse.py

--- cairo/ExtractInfoFromResponse.py
+++ cairo/ExtractInfoFromResponse.py
@@ -1,7 +1,19 @@
 import streamlit
-from cairo.convertGameDate import convertDate
 from cairo.TeamLogo import teamLogo
 
+class Date:
+    month = 0
+    day = 0
+    year = 0
+
+#converts gameDate from JSON response into MM/DD/YYYY
+def convertDate(date):
+    gameDate = Date()
+    gameDate.year = date[0:4]
+    gameDate.month = date[4:6]
+    gameDate.day = date[6:]
+
+    return f"{gameDate.month}/{gameDate.day}/{gameDate.year}"
 
 # extract and display info from response
 def extractAndDisplayInfo(response):
